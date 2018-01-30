0522f00f880ebb754a87ede96d2d8618db1c61e3
Michael du Breuil <wicked.shell.scripts@gmail.com>
2018-01-29 18:28:36 -0700

    AP_Script: Demo binding send_text

--- libraries/AP_Script/AP_Script.cpp
+++ libraries/AP_Script/AP_Script.cpp
@@ -8,12 +8,20 @@ extern "C" {
 
 extern const AP_HAL::HAL& hal;
 
+int l_send_text(lua_State *L) {
+    const char* str = lua_tostring(L, 1);
+    gcs().send_text(MAV_SEVERITY_INFO, str);
+    return 0;
+}
+
 AP_Script::AP_Script(void) {
     lua_State *state = luaL_newstate();
     luaL_openlibs(state);
-    luaL_dostring(state,"local a=5 local b=10 local c=a+b return c");
+    lua_pushcfunction(state, l_send_text);
+    lua_setglobal(state, "sendText");
+    luaL_dostring(state,"local a=5 local b=10 local c=a+b sendText(string.format(\"testing math %d\", c)) return c+2");
     int r = luaL_checkinteger(state, -1);
-    gcs().send_text(MAV_SEVERITY_INFO, "Got %d", r);
+    gcs().send_text(MAV_SEVERITY_INFO, "Return value of %d", r);
     lua_close(state);
 }
 
