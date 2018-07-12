33d7bfe54cfa45eb4fa8032610b023d49e85aa60
Buzz <davidbuzz@gmail.com>
2018-07-12 16:50:02 +1000

    bump version file for -rfdesign branch

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V3.8.5"
+#define THISFIRMWARE "ArduPlane V3.8.5-RFDesign"
 #define FIRMWARE_VERSION 3,8,5,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #ifndef GIT_VERSION
