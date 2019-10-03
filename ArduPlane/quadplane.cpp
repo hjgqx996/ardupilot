eb1ef10cb006495f220027b749f95a47ceac2e2d
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-10-02 18:14:34 -0700

    Plane: Resore old default behaviour for throttle curves
    The change in this made QStabilize behave very differently then it had,
    which is quite shocking in a test flight.

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -415,7 +415,7 @@ const AP_Param::GroupInfo QuadPlane::var_info2[] = {
     // @Range: 0 1
     // @Increment: .1
     // @User: Advanced
-    AP_GROUPINFO("THROTTLE_EXPO", 10, QuadPlane, throttle_expo, 0.2),
+    AP_GROUPINFO("THROTTLE_EXPO", 10, QuadPlane, throttle_expo, 0.0),
 
     // @Param: ACRO_RLL_RATE
     // @DisplayName: QACRO mode roll rate
@@ -983,12 +983,16 @@ float QuadPlane::get_pilot_throttle()
     // normalize to [0,1]
     throttle_in /= plane.channel_throttle->get_range();
 
-    // get hover throttle level [0,1]
-    float thr_mid = motors->get_throttle_hover();
-    float thrust_curve_expo = constrain_float(throttle_expo, 0.0f, 1.0f);
+    if (is_positive(throttle_expo)) {
+        // get hover throttle level [0,1]
+        float thr_mid = motors->get_throttle_hover();
+        float thrust_curve_expo = constrain_float(throttle_expo, 0.0f, 1.0f);
 
-    // this puts mid stick at hover throttle
-    return throttle_curve(thr_mid, thrust_curve_expo, throttle_in);;
+        // this puts mid stick at hover throttle
+        return throttle_curve(thr_mid, thrust_curve_expo, throttle_in);;
+    } else {
+        return throttle_in;
+    }
 }
 
 /*
